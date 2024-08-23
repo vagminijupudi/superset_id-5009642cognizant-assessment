33519373d693740838e6504f574033332d7c6870
vagminijupudi <109346771+vagminijupudi@users.noreply.github.com>
2024-08-23 14:29:36 +0530

    Delete Q5 BookstoreAPI directory

--- Q5 BookstoreAPI/src/main/java/com/example/BookstoreAPI/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BookstoreAPI.repository;
-
-import com.example.BookstoreAPI.models.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
